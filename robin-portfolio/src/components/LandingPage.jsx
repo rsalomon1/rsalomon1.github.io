6759646c8ac8c6b944de40d4aa5ac3c2ae6c0e65
Robin <robin.lynne.salomon@gmail.com>
2023-04-30 02:32:08 -0400

    Added menu item text to landing page

--- robin-portfolio/src/components/LandingPage.jsx
+++ robin-portfolio/src/components/LandingPage.jsx
@@ -9,7 +9,12 @@ function LandingPage() {
     <div>
     <div className='page-container'>
     <p className="robin-salomon-name" >Robin Salomon</p>
-
+    <div className='landing-page-menu-container'>
+      
+    <p className='menu-item'>About</p>
+    <p className='menu-item'>Services</p>
+    <p className='menu-item'>Contact</p>
+    </div>
     </div>
     </div>
   )
