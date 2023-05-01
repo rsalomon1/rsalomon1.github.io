23b671cdc354cde8c014609834a87bf8f5a64112
Robin <robin.lynne.salomon@gmail.com>
2023-05-01 16:15:06 -0400

    Added ContactMe page, working on making fully responsive.

--- robin-portfolio/src/components/ContactMe.jsx
+++ robin-portfolio/src/components/ContactMe.jsx
@@ -1,13 +1,45 @@
-import React from 'react'
-import NavBar from './NavBar'
+import React from "react";
+import NavBar from "./NavBar";
 
 function ContactMe() {
   return (
-    <div>
-      <NavBar/>
-    <div>I can be reached at ...</div>
+    <div className="contact-me-page">
+      <NavBar />
+      <img className="icon" src={require("../images/email_icon.png")}></img>
+      <p className="header-text">Contact Me</p>
+      <p className="p-font">
+        I am always open to new business opportunities. If you are interested in
+        working with me, or if you have any questions, I welcome you to send me
+        a message at the email listed below. Feel free to also connect with me
+        on LinkedIn. I am happy to share references upon request.
+      </p>
+      <div className="bullets-container">
+        <p style={{ color: "rgb(220,127,50)" }} className="bullet-points">
+          ⸻
+        </p>
+        <p style={{ color: "rgb(220,127,50)" }} className="p-font">
+          <span style={{ color: "gray", marginRight: "5px" }}>Email:</span>{" "}
+          robin.lynne.salomon@gmail.com
+        </p>
+        <p className="p-font">
+          <span>
+            Connect with me on LinkedIn{" "}
+            <a
+              style={{ color: "rgb(220,127,50)", textDecoration:"none" }}
+              target="_blank"
+              href="https://www.linkedin.com/in/robin-l-salomon/"
+            >
+              {" "}
+              here.
+            </a>
+          </span>
+        </p>
+        <p style={{ color: "rgb(220,127,50)" }} className="bullet-points">
+          ⸻
+        </p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default ContactMe
+export default ContactMe;
