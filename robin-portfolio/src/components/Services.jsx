dfde937ddad657ca7ad4ced0ebe675ad4cd37573
Robin <robin.lynne.salomon@gmail.com>
2023-05-01 03:05:00 -0400

    Added icon and header to Services page

--- robin-portfolio/src/components/Services.jsx
+++ robin-portfolio/src/components/Services.jsx
@@ -4,7 +4,10 @@ function Services() {
   return (
     <div>
         <NavBar />
-    <div>Services</div>
+        <div>
+        <img className="icon" src={require("../images/handshake_icon.png")}></img>
+        <p className="header-text">How I Can Help</p>
+        </div>
     </div>
   )
 }
